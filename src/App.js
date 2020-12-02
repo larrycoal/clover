3c40eaac6debe0d527ba176ae1bb97ca0f59ade7
Christian Otu <christianotu.d@gmail.com>
2020-12-02 05:21:23 +0100

    Removed repetitive components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { Home } from "./pages/Home";
 import AboutUs from './pages/AboutUs'
 import Help from './pages/Help'
 import Layout from "./Layout";
-import "./App.css";
-import {Switch,Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+
 const App = () => {
   return (
     <React.Fragment>
       <Layout>
         <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/about-us" component={AboutUs}/>
-        <Route exactpath="/help" component={Help}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={AboutUs} />
+          <Route exactpath="/help" component={Help} />
         </Switch>
       </Layout>
     </React.Fragment>
