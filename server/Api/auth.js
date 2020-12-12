fc3a7947eb3c04b5314438cffdadbcccdb68b706
Olayiwola Olanrewaju <larry_coal@outlook.com>
2020-12-12 15:07:08 +0100

    login and register logic

--- server/Api/auth.js
+++ server/Api/auth.js
@@ -1,13 +1,34 @@
 const express =require('express')
-
+const db = require('../Database/config')
 const api = express.Router()
-
+const bcrypt = require("bcryptjs")
 
 api.post("/login",(req,res)=>{
-console.log(req.body)
+    let {email,password} = req.body 
+db.query("SELECT * FROM users WHERE email = ?",[email],(err,result)=>{
+    if (result[0].length>0){
+       bcrypt.compare(result[0].password,password)
+       res.json(result)
+    }
+})
 })
 api.post("/register",(req,res)=>{
-    console.log(req.body)
+    let {email,password,name,id} = req.body
+    db.query("SELECT email FROM users WHERE email = ?",[email],(err,result)=>{
+        if (result[0]){
+            res.json({message:"User Already exist"})
+        }else{
+            bcrypt.hash(password,8 ,(hash)=>{
+                db.query("INSERT INTO users SET ?",{id,name,email,password:hash},(err,result)=>{
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log(result)
+                    }
+                })
+            })
+        }
+    })
 })
 
 module.exports = api
